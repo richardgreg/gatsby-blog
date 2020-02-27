0fdcd5d74b1d4ac2e79cfd0fc79c42d612481a1b
Richard O. Gregory <richardgrecoson@gmail.com>
2020-02-27 19:30:39 +0100

    Config Markdowns
    Configure the markdown-pages in gatsby-config.js and enable
    index.js to display markdown pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+// Destructure 'data' object from graphql
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>Richard's Musings</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node}) => (
+          <div key={node.id}>
+            <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+         query {
+           allMarkdownRemark {
+             edges {
+               node {
+                 frontmatter {
+                   description
+                   date
+                   title
+                 }
+                 id
+                 excerpt
+               }
+             }
+             pageInfo {
+               perPage
+             }
+             totalCount
+           }
+         }
+       `
