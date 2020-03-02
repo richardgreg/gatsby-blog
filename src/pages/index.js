3adaf3ec03170d7df1cfd7e254d9e287ca153253
Richard O. Gregory <richardgrecoson@gmail.com>
2020-03-02 02:14:37 +0100

    Config styled componets and style pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,37 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`;
+
 // Destructure 'data' object from graphql
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <div>
       <h1>Richard's Musings</h1>
       <h4>{data.allMarkdownRemark.totalCount}</h4>
-      {
-        data.allMarkdownRemark.edges.map(({node}) => (
-          <div key={node.id}>
-            <span>{node.frontmatter.title} - {node.frontmatter.date}</span>
-            <p>{node.excerpt}</p>
-          </div>
-        ))
-      }
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
   </Layout>
 )
@@ -27,23 +39,26 @@ const IndexPage = ({data}) => (
 export default IndexPage
 
 export const query = graphql`
-         query {
-           allMarkdownRemark {
-             edges {
-               node {
-                 frontmatter {
-                   description
-                   date
-                   title
-                 }
-                 id
-                 excerpt
-               }
-             }
-             pageInfo {
-               perPage
-             }
-             totalCount
-           }
-         }
-       `
+    query {
+      allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+        edges {
+          node {
+            frontmatter {
+              description
+              date
+              title
+            }
+            fields{
+              slug
+            }
+            id
+            excerpt
+          }
+        }
+        pageInfo {
+          perPage
+        }
+        totalCount
+      }
+    }
+  `
